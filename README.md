c1fab2beae8e0b575235315180af6a2d14740853
florianakos <florian.akos.szabo@gmail.com>
2018-12-03 23:59:45 +0100

    Init

---
+++ README.md
@@ -0,0 +1,33 @@
+## AWS Credentials Helper Library ##
+
+This is a simple Go library that wraps the "AWS GO SDK" library to provide a way to create / list / delete AWS EC2 Security KeyPairs and Security Groups that allow through SSH by defualt.
+
+### Example Usage: ###
+
+```go
+awshelper.CreateEC2KeyPair("aws_region", "key_name")
+```
+The above code creates a new KeyPair and saves the secret key to ``~/.ssh/ `` directory on UNIX systems. It returns an error if something went wrong.
+
+
+```go
+awshelper.ListAllEC2KeyPairs("aws_region")
+```
+The above code returns a array of pointers to ec2.KeyPairInfo or an error if something goes wrong while querying the region for all security keys available.
+
+
+```go
+awshelper.DeleteEC2KeyPair("aws_region", "keypair_name")
+```
+The above code deletes a key in the specifid region if it exists, otherwise gives an error.
+
+
+```go
+awshelper.CreateSecurityGroupForSSH("aws_region", "group_name", "group_description")
+```
+The above code creates a security group with name and description unless the name already exists. It returns a string which contains the sgID of the newly create security group or an error if something went wrong.
+
+```go
+awshelper.ListAllSecurityGroups("aws_region", "sgID")
+```
+The above function lists all security groups in a region if the "sgID" string was empty, otherwise lists all info about the security group with the given ID.
---
+++ awshelper.go
@@ -0,0 +1,228 @@
+package awshelper
+
+import (
+	"fmt"
+	"errors"
+    _"os"
+    "os/user"
+    "io/ioutil"
+
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/ec2"
+)
+
+var AllRegions = []string{
+	"us-east-2", 		//US East (Ohio) 	
+	"us-east-1",		//US East (N. Virginia)
+	"us-west-1", 		//US West (N. California)
+	"us-west-2", 		//US West (Oregon)
+	"ap-south-1", 		//Asia Pacific (Mumbai)
+	"ap-northeast-3", 	//Asia Pacific (Osaka-Local)
+	"ap-northeast-2", 	//Asia Pacific (Seoul)
+	"ap-southeast-1", 	//Asia Pacific (Singapore)
+	"ap-southeast-2", 	//Asia Pacific (Sydney)
+	"ap-northeast-1", 	//Asia Pacific (Tokyo)
+	"ca-central-1", 	//Canada (Central) 	
+	"cn-north-1", 		//China (Beijing)
+	"cn-northwest-1", 	//China (Ningxia)
+	"eu-central-1", 	//EU (Frankfurt) 	
+	"eu-west-1", 		//EU (Ireland)
+	"eu-west-2", 		//EU (London)
+	"eu-west-3", 		//EU (Paris)
+	"sa-east-1", 		//South America (São Paulo) 	
+	"us-gov-east-1", 	//AWS GovCloud (US-East) 	
+	"us-gov-west-1", 	//AWS GovCloud (US)
+}
+
+// checks if the supplied string is a valud AWS EC2 region name
+func isValidEC2Region(compare string) bool {
+	
+	for _, value := range AllRegions {
+		if value == compare {
+			return true
+		}
+	}
+	return false
+
+}
+
+// helper function to validate region and open SVC session to EC2
+func openServiceClientEC2(region string) (*ec2.EC2, error) {
+
+	// validate region string
+	if !isValidEC2Region(region) {
+		return nil, errors.New("Invalid region name!")
+	}
+	// open session in region
+	sess, err := session.NewSession(&aws.Config{ Region: aws.String(region)}, )
+	// check erros if any, otherwise return the session pointer
+    if err != nil {
+    	return nil, err
+    } else {
+    	return ec2.New(sess), nil
+    }
+
+}
+
+
+// Creates a new AWS SSH Secret key and puts the secret content in a file in ~/.ssh/ dir
+func CreateEC2KeyPair(region string, keyname string) error {
+
+	usr, err := user.Current()
+    if err != nil {
+        return errors.New("Unable to determine UNIX user HOME directory!")
+    }
+
+	svc, err := openServiceClientEC2(region)
+   	if err != nil {
+   		return err
+   	}
+
+    newKey, err := svc.CreateKeyPair(&ec2.CreateKeyPairInput{ KeyName: aws.String(keyname), })
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok && aerr.Code() == "InvalidKeyPair.Duplicate" {
+            return errors.New("Keypair already exists with that name in that region!")
+        }
+        return errors.New("Unable to create keypair!")
+    }
+
+	err = ioutil.WriteFile(usr.HomeDir + "/.ssh/"+keyname+".pem", 
+    						[]byte(*newKey.KeyMaterial), 0600)
+    if err != nil {
+    	return errors.New("Error writing secret key to file!")
+    }
+
+	return nil
+}
+
+// function to list all AWS KeyPairs in region specified
+func ListAllEC2KeyPairs(region string) ([]*ec2.KeyPairInfo, error) {
+	
+	svc, err := openServiceClientEC2(region)
+   	if err != nil {
+   		return nil, err
+   	}
+    result, err := svc.DescribeKeyPairs(nil)
+    if err != nil {
+    	return nil, errors.New(fmt.Sprintf("Unable to get key pairs, %v", err))
+    }
+    return result.KeyPairs, nil
+
+}
+
+// function to delete specified key in specified region
+func DeleteEC2KeyPair(region string, keyname string) error {
+
+	svc, err := openServiceClientEC2(region)
+   	if err != nil {
+   		return err
+   	}
+   	_, err = svc.DeleteKeyPair(&ec2.DeleteKeyPairInput{
+        KeyName: aws.String(keyname),
+    })
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok && aerr.Code() == "InvalidKeyPair.Duplicate" {
+            return errors.New(fmt.Sprintf("Key pair %q does not exist.", keyname))
+        }
+        return errors.New(fmt.Sprintf("Unable to delete key pair: %s, %v.", keyname, err))
+    }
+    return nil
+
+}
+
+
+// Function to create security group that allows SSN through on TCP/22
+// returns (string, error) that point to SG-ID and error if any during execution
+func CreateSecurityGroupForSSH(region string, name string, description string) (string, error) {
+
+	if len(name) == 0 || len(description) == 0 {
+        return "", errors.New("Invalid Group name or Group Description supplied")
+    }
+
+   	svc, err := openServiceClientEC2(region)
+   	if err != nil {
+   		return "", err
+   	}
+
+   	// Get a list of VPCs so we can associate the group with the first VPC.
+    availableVPCs, err := svc.DescribeVpcs(nil)
+    if err != nil {
+        return "", errors.New(fmt.Sprintf("Unable to describe VPCs, %v", err))
+    }
+    if len(availableVPCs.Vpcs) == 0 {
+        return "", errors.New("No VPCs found to associate security group with.")
+    }
+    vpcID := aws.StringValue(availableVPCs.Vpcs[0].VpcId)
+
+    // Create the security group with the VPC, name and description.
+    createSGResult, err := svc.CreateSecurityGroup(&ec2.CreateSecurityGroupInput{
+        GroupName:   aws.String(name),
+        Description: aws.String(description),
+        VpcId:       aws.String(vpcID),
+    })
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok {
+            switch aerr.Code() {
+            case "InvalidVpcID.NotFound":
+                return "", errors.New(fmt.Sprintf("Unable to find VPC with ID %q.", vpcID))
+            case "InvalidGroup.Duplicate":
+                return "", errors.New(fmt.Sprintf("Security group %q already exists.", name))
+            }
+        }
+        return "", errors.New(fmt.Sprintf("Unable to create security group %q, %v", name, err))
+    }
+
+    // fmt.Printf("Created security group %s with VPC %s.\n",
+    //     aws.StringValue(createSGResult.GroupId), vpcID)
+
+    newSGID := createSGResult.GroupId
+
+    // authorize the created security group for SSH ingress
+    _, err = svc.AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
+        GroupName: aws.String(name),
+        IpPermissions: []*ec2.IpPermission{
+            (&ec2.IpPermission{}).
+                SetIpProtocol("tcp").
+                SetFromPort(22).
+                SetToPort(22).
+                SetIpRanges([]*ec2.IpRange{
+                    (&ec2.IpRange{}).
+                        SetCidrIp("0.0.0.0/0"),
+                }),
+        },
+    })
+    if err != nil {
+        return "", errors.New(fmt.Sprintf("Unable to set security group %q ingress, %v", name, err))
+    }
+    // return SG-ID and nil error
+    return *newSGID, nil
+
+}
+
+// function to list all security groups, if sgID is "" then it lists all in the region
+func ListAllSecurityGroups(region string, sgID string) ([]*ec2.SecurityGroup, error) {
+	
+	svc, err := openServiceClientEC2(region)
+   	if err != nil {
+   		return nil, err
+   	}
+   	
+   	result, err := svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+   		GroupIds: aws.StringSlice([]string{sgID,}),
+   	})
+	if err != nil {
+	    if aerr, ok := err.(awserr.Error); ok {
+	        switch aerr.Code() {
+	        case "InvalidGroupId.Malformed":
+	            fallthrough
+	        case "InvalidGroup.NotFound":
+	            return nil, errors.New(fmt.Sprintf("%s.", aerr.Message()))
+	        }
+	    }
+	    return nil, errors.New(fmt.Sprintf("Unable to get descriptions for security groups, %v", err))
+	}
+	return result.SecurityGroups, nil
+
+}
